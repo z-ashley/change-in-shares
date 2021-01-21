0c2286ca54c3fdd5da89060310e9d0f6fd2ad19a
z-ashley <68879962+sheriasblue@users.noreply.github.com>
2021-01-21 14:52:11 -0500

    d

---
+++ convert_csv.py
@@ -0,0 +1,12 @@
+import csv
+import openpyxl
+
+wb = openpyxl.Workbook()
+ws = wb.active
+
+with open('arkg_holdings.csv') as f:
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        ws.append(row)
+
+wb.save('arkg_holdings1.xlsx')
---
+++ update_shares.py
@@ -0,0 +1,11 @@
+from openpyxl import load_workbook
+arkgWB = load_workbook(filename = 'arkg_holdings1.xlsx')
+arkgSheet = arkgWB.active
+# print(sheet["C"])
+sharesWB = load_workbook(filename = 'shareschange.xlsx')
+sharesSheet = sharesWB.active
+
+print(arkgSheet.max_row)
+
+# for rowNum in range(1, arkgSheet.max_row): 
+#     sharesSheet.cell(row=rowNum, column=1).value = arkgSheet.cell(row=rowNum, column=1).value
