ca456b2dac7e93f08efd2dbd5a950d2455b091f9
z-ashley <52079959+z-ashley@users.noreply.github.com>
2021-01-21 19:15:31 -0500

    can copy new values

--- update_shares.py
+++ update_shares.py
@@ -3,19 +3,24 @@ from openpyxl import load_workbook
 testWB = load_workbook(filename = 'tester.xlsx')
 testSheet = testWB.active
 
-arkgWB = load_workbook(filename = 'arkg_holdings1.xlsx')
+arkgWB = load_workbook(filename = 'arkg_holdings2.xlsx')
 arkgSheet = arkgWB.active
 
 sharesWB = load_workbook(filename = 'shareschange.xlsx')
 sharesSheet = sharesWB.active
 
 
-counter = 1
-for row in sharesSheet.iter_rows(1):
+sharesSheet.insert_cols(idx=3)
+
+counter = 1 
+while counter < arkgSheet.max_row:
     stockName = arkgSheet.cell(row=counter, column=3).value
-    for cell in row:
+    for cell in sharesSheet['A']:
         if cell.value == stockName:
             sharesSheet.cell(row=cell.row, column=3).value = arkgSheet.cell(row=counter, column=6).value
     counter+=1
 
+del
+
 sharesWB.save(filename="shareschange.xlsx")
+
